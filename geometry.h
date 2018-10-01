75314e4a707eb0caba5ed08ab87bd7fc28fff3de
konstantinos katrioplas <kokatrio@gmail.com>
2018-10-01 22:27:57 +0300

    cross product, area

--- geometry.h
+++ geometry.h
@@ -1,22 +1,42 @@
-#include <iostream>
+#include <math.h>
 
-struct Point2D
+struct Point3D
 {
 	double x;
 	double y;
+	double z;
 };
 
-template <typename Point>
-struct Vector2D
+struct Vector3D
 {
-	Vector2D(Point& a, Point& b) : x(b.x - a.x), y(b.y - a.y) {}
+	Vector3D() = default;
+	Vector3D(Point3D& a, Point3D& b) : x(b.x - a.x), y(b.y - a.y), z(b.z - a.z) {}
+	Vector3D(double x, double y, double z) : x(x), y(y), z(z) {}
 
 	double x;
 	double y;
+	double z;
+
+	double norm()
+	{
+		return std::sqrt(x*x + y*y + z*z);
+	}
+
+	bool operator==(const Vector3D v)
+	{
+		return (v.x == x && v.y == y && v.z == z) ? true : false;
+	}
 };
 
-
-
+template <typename Vector>
+Vector cross(const Vector& v0, const Vector& v1)
+{
+	Vector v_res{};
+	v_res.x = v0.y * v1.z - v1.y * v0.z;
+	v_res.y = v0.z * v1.x - v0.x * v1.z;
+	v_res.z = v0.x * v1.y - v1.x * v0.y;
+	return v_res;
+}
 
 template <typename Point>
 class Triangle
@@ -24,14 +44,14 @@ class Triangle
 public:
 	Triangle(Point& p0, Point& p1, Point& p2) : p0(p0), p1(p1), p2(p2) {}
 
-/*
-	area()
+	double area()
 	{
-
+		// cross two vectors, starting at p0
+		Vector3D v0{p0, p1};
+		Vector3D v1{p0, p2};
+		Vector3D cp = cross(v0, v1);
+		return cp.norm() / 2;
 	}
-*/
-
-
 
 private:
 	Point p0;
--- main.cpp
+++ main.cpp
@@ -1,14 +1,38 @@
+#include <assert.h>
+#include <iostream>
 #include "geometry.h"
+
+void test_cross_product()
+{
+  using Point = Point3D;
+  using Vector = Vector3D;
+  Point o{0, 0, 0};
+  Point a{2, 0, 0};
+  Point b{2, 3, 2};
+  Vector v0{o, a};
+  Vector v1{o, b};
+  Vector cp = cross(v0, v1);
+
+  Vector v{0, -4, 6};
+  assert(cp == v);
+}
+
+void test_area()
+{
+  using Point = Point3D;
+  Point a{0, 0, 0};
+  Point b{2, 0, 0};
+  Point c{2, 3, 0};
+
+  Triangle<Point> t{a, b, c};
+  assert(t.area() == 3.0);
+}
+
 
 int main()
 {
-
-  Point2D a{0, 1};
-  Point2D b{-2, 3.1};
-
-  Vector2D<Point2D> v0{a,b};
-  std::cout << v0.x << " " << v0.y << '\n';
-
+  test_cross_product();
+  test_area();
 
   return 0;
 }
